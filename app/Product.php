73127657ec32bd8918e7633ce5962cfca7a9dfae
Armino Popp <devel@boioiong.com>
2020-06-27 14:07:50 +0300

    Added product model and migration

---
+++ app/Product.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+
+    protected $guarded = ['id'];
+    
+    
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);        
+    }
+}
---
+++ database/migrations/2020_06_27_192814_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->string('name', 255);
+            $table->string('slug', 255)->unique();
+            $table->unsignedBigInteger('category_id')->index()->default(1);
+            $table->text('description')->nullable();
+            $table->string('image', 255)->nullable();
+            $table->decimal('price', 6, 2)->default(0);
+
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
