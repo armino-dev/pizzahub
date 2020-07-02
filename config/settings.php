7739c1780eb5b069e05ce5468d798141319d594a
Armino Popp <devel@boioiong.com>
2020-07-02 22:10:07 +0300

    Added config settings to hold some global data

---
+++ config/settings.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'currencies' => [
+        'eur' => ['value' => 1, 'symbol' => "€"],
+        'usd' => ['value' => 1.12, 'symbol' => "$"],
+    ],
+    'vat' => 19,    
+    'delivery_cost' => ['bucharest' => 2, 'bragadiru' => 4],
+];
