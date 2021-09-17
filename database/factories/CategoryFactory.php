c61e1cf887056d73013847b6ec6edb9535a5be58
Armino Popp <devel@boioiong.com>
2021-09-17 09:46:35 +0300

    Fix namespaces

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -1,9 +1,12 @@
 <?php
 
+namespace Database\Factories;
+
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\Models\Category;
 use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 $factory->define(Category::class, function (Faker $faker) {
     $name = $faker->unique()->safeColorName;
--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -1,9 +1,13 @@
 <?php
 
+namespace Database\Factories;
+
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
+use App\Models\Category;
 use App\Models\Product;
 use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 $factory->define(Product::class, function (Faker $faker) {
     $name = $faker->unique()->colorName;
@@ -12,7 +16,7 @@ $factory->define(Product::class, function (Faker $faker) {
         'name' => $name,
         'slug' => Str::slug($name),
         'description' =>  $faker->paragraph,
-        'category_id' => factory(App\Models\Category::class),
+        'category_id' => Category::factory(),
         'image' => $faker->imageUrl,
         'price' => $faker->randomFloat,
         'best_seller' => $faker->boolean(20),
