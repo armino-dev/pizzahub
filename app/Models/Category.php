daee920a6f2573e92ffb4b9ea5baf81e496c7365
Armino Popp <armino.popp@gmail.com>
2021-11-04 12:58:25 +0200

    Added algolia search (#32)

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
 
 /**
  * App\Models\Category.
@@ -30,7 +31,7 @@ use Illuminate\Database\Eloquent\Model;
  */
 class Category extends Model
 {
-    use HasFactory;
+    use HasFactory, Searchable;
 
     public function getRouteKeyName()
     {
