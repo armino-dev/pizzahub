7ee26771d39928ab6bb9446c9d340927ad3be8fa
Armino Popp <armino.popp@tradeshift.com>
2022-01-19 14:13:29 +0200

    New translations auth.php (Romanian)

---
+++ resources/lang/ro_RO/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Aceste informații de autentificare nu corespund înregistrărilor noastre.',
+    'throttle' => 'Prea multe tentative de autentificare. Vă rugăm să încercaţi din nou în :seconds secunde.',
+
+];
