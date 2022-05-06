1106318d92a31fa0161bf1f619925663b95a5ecd
Armino Popp <devel@boioiong.com>
2022-05-06 19:07:29 +0300

    Add redis

--- app/Http/Controllers/Frontend/CategoryController.php
+++ app/Http/Controllers/Frontend/CategoryController.php
@@ -5,15 +5,24 @@ namespace App\Http\Controllers\Frontend;
 use App\Http\Controllers\Controller;
 use App\Models\Category;
 use App\Models\Product;
+use Illuminate\Support\Facades\Cache;
 
 class CategoryController extends Controller
 {
     public function show(?Category $category = null)
     {
         $user = auth()->user();
-        $categories = $category->exists ? Category::all() : collect();
 
-        $products = Product::where('category_id', $category['id'])->latest()->get();
+        $allCategories = Cache::remember('categories', now()->addDay(), function () {
+            return Category::all();
+        });
+
+        $categories = $category->exists ? $allCategories : collect();
+
+        $cacheKey = 'products-on-category-' . $category['id'];
+        $products = Cache::remember($cacheKey, now()->addDay(), function ($category) {
+            return Product::where('category_id', $category['id'])->latest()->get();
+        });
 
         return view('frontend.category', compact('user', 'categories', 'category', 'products'));
     }
